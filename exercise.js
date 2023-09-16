b18f54f6beff52a488859f3be8a8c53630895ae4
udinocera <udinocera@gmail.com>
2023-09-16 16:31:30 +0200

    fine

--- exercise.js
+++ exercise.js
@@ -3,3 +3,15 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function getUser(data){
+ try {
+  const savedUser = JSON.parse(localStorage.getItem(data));
+  console.log("Utente recuperato", savedUser);
+ } catch(error) {
+  console.error(error);
+ }
+}
+
+
+getUser("user");
